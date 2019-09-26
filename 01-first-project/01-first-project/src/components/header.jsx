5a08d7c2b84d180377c5909b2e6d0f1d6c13735b
Dima <dim.kry.lis@gmail.com>
2019-09-26 14:01:34 +0300

    first components (header, navbar, profile) created

---
+++ 01-first-project/01-first-project/src/components/header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+          <img src="http://cdn-images.mailchimp.com/template_images/logo_nmcux.png"/>
+        </header>
+    )
+}
+
+export default Header;
